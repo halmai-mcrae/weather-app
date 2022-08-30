7c7131cdb713ed0f970ec1a5edd71b38a31dfa99
halmai-mcrae <hello@halmaimcrae.co.nz>
2022-08-30 15:09:29 +1200

    Add city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '1d7d71b356msh502a87f39349f6dp164b25jsn9fb9346b061c',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
