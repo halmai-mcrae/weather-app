0eb05ee66d5e8347afd263c85a87cad21d83260c
halmai-mcrae <hello@halmaimcrae.co.nz>
2022-08-30 14:34:21 +1200

    Dummy search component initialised

---
+++ src/components/search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Search;
