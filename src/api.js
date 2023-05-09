56e51e58c828cb847fcde5a69e7db3f7576e30fc
Halmai McRae <halmaimcrae@Halmais-MacBook-Pro.local>
2023-05-10 09:57:31 +1200

    Add .env

--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '1d7d71b356msh502a87f39349f6dp164b25jsn9fb9346b061c',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+    'X-RapidAPI-Key':
+      '1d7d71b356msh502a87f39349f6dp164b25jsn9fb9346b061c',
+    'X-RapidAPI-Host':
+      'wft-geo-db.p.rapidapi.com',
   },
 }
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+export const GEO_API_URL =
+  process.env.REACT_APP_GEO_API_URL
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_URL =
+  process.env.REACT_APP_WEATHER_API_URL
 
-export const WEATHER_API_KEY = '7e475f1659df86abe97baf22bd76e1a4'
+export const WEATHER_API_KEY =
+  process.env.REACT_APP_WEATHER_API_KEY
